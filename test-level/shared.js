863fc547a2d412248780db9b0f8607cb2e2c3379
videogame hacker <half-kh-hacker@hackery.site>
2023-08-07 17:03:39 +0100

    Firefox doesn't support JSON imports for some reason

--- test-level/shared.js
+++ test-level/shared.js
@@ -1,5 +1,5 @@
 import { LevelSchema } from "@dreamlab.gg/core";
-import TestLevel from "./test-level.json" assert { type: "json" };
+import TestLevel from "./test-level.js";
 
 export const sharedInit = async (game) => {
   await game.load(LevelSchema.parse(TestLevel));
