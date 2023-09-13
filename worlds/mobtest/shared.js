9f91dc358930699c87d90f180ad113f32814ea24
CodedRed <devcodedred@gmail.com>
2023-09-13 14:36:40 -0500

    level 2

--- worlds/mobtest/shared.js
+++ worlds/mobtest/shared.js
@@ -2,13 +2,14 @@ import { createElevator } from './entities/elevator.js'
 import { createHittableMob } from './entities/hittableMob.js'
 import { balls } from './level/ball-floor.js'
 import { level as level1 } from './level/level1.js'
+import { level2 } from './level/level2.js'
 
 /** @type {import('@dreamlab.gg/core/sdk').InitShared} */
 export const sharedInit = async game => {
   game.register('@dreamlab/Hittable', createHittableMob)
   game.register('@dreamlab/Elevator', createElevator)
 
-  await game.spawnMany(...level1, ...balls)
+  await game.spawnMany(...level1, ...balls, ...level2)
 
 }
 
