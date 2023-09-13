7a0d46263b6013031ba6f1301b962ae0ec5001f2
CodedRed <devcodedred@gmail.com>
2023-09-13 14:27:47 -0500

    implemented more levels + elevator entity

---
+++ worlds/mobtest/level/ball-floor.js
@@ -0,0 +1,163 @@
+/** @type {import('@dreamlab.gg/core').LooseSpawnableDefinition[]} */
+export const balls = [
+  {
+    entity: '@dreamlab/Solid', // bottom floor
+    args: [5_000, 50],
+    transform: {
+      position: { x: 0, y: 2_000 },
+      rotation: 0,
+    },
+  },
+  {
+    entity: '@dreamlab/BouncyBall',
+    args: [50],
+    transform: {
+      position: { x: 0, y: 1_750 },
+      rotation: 90,
+    },
+  },
+  {
+    entity: '@dreamlab/BouncyBall',
+    args: [50],
+    transform: {
+      position: { x: 0, y: 1_750 },
+      rotation: 90,
+    },
+  },
+  {
+    entity: '@dreamlab/BouncyBall',
+    args: [50],
+    transform: {
+      position: { x: 0, y: 1_750 },
+      rotation: 90,
+    },
+  },
+  {
+    entity: '@dreamlab/BouncyBall',
+    args: [50],
+    transform: {
+      position: { x: 0, y: 1_750 },
+      rotation: 90,
+    },
+  },
+  {
+    entity: '@dreamlab/BouncyBall',
+    args: [50],
+    transform: {
+      position: { x: 0, y: 1_750 },
+      rotation: 90,
+    },
+  },
+  {
+    entity: '@dreamlab/BouncyBall',
+    args: [50],
+    transform: {
+      position: { x: 0, y: 1_750 },
+      rotation: 90,
+    },
+  },
+  {
+    entity: '@dreamlab/BouncyBall',
+    args: [50],
+    transform: {
+      position: { x: 0, y: 1_750 },
+      rotation: 90,
+    },
+  },
+  {
+    entity: '@dreamlab/BouncyBall',
+    args: [50],
+    transform: {
+      position: { x: 0, y: 1_750 },
+      rotation: 90,
+    },
+  },
+  {
+    entity: '@dreamlab/BouncyBall',
+    args: [50],
+    transform: {
+      position: { x: 0, y: 1_750 },
+      rotation: 90,
+    },
+  },
+  {
+    entity: '@dreamlab/BouncyBall',
+    args: [50],
+    transform: {
+      position: { x: 0, y: 1_750 },
+      rotation: 90,
+    },
+  },
+  {
+    entity: '@dreamlab/BouncyBall',
+    args: [50],
+    transform: {
+      position: { x: 0, y: 1_750 },
+      rotation: 90,
+    },
+  },
+  {
+    entity: '@dreamlab/BouncyBall',
+    args: [50],
+    transform: {
+      position: { x: 0, y: 1_750 },
+      rotation: 90,
+    },
+  },
+  {
+    entity: '@dreamlab/BouncyBall',
+    args: [50],
+    transform: {
+      position: { x: 0, y: 1_750 },
+      rotation: 90,
+    },
+  },
+  {
+    entity: '@dreamlab/BouncyBall',
+    args: [50],
+    transform: {
+      position: { x: 0, y: 1_750 },
+      rotation: 90,
+    },
+  },
+  {
+    entity: '@dreamlab/BouncyBall',
+    args: [50],
+    transform: {
+      position: { x: 0, y: 1_750 },
+      rotation: 90,
+    },
+  },
+  {
+    entity: '@dreamlab/BouncyBall',
+    args: [50],
+    transform: {
+      position: { x: 0, y: 1_750 },
+      rotation: 90,
+    },
+  },
+  {
+    entity: '@dreamlab/BouncyBall',
+    args: [50],
+    transform: {
+      position: { x: 0, y: 1_750 },
+      rotation: 90,
+    },
+  },
+  {
+    entity: '@dreamlab/BouncyBall',
+    args: [50],
+    transform: {
+      position: { x: 0, y: 1_750 },
+      rotation: 90,
+    },
+  },
+  {
+    entity: '@dreamlab/BouncyBall',
+    args: [50],
+    transform: {
+      position: { x: 0, y: 1_750 },
+      rotation: 90,
+    },
+  },
+]
