a21ac0215b6a027bce1438c10aec462ad415e26e
CodedRed <devcodedred@gmail.com>
2023-09-18 13:51:12 -0500

    format

--- worlds/mobtest/level/shared/ball-floor.js
+++ worlds/mobtest/level/shared/ball-floor.js
@@ -1,38 +1,37 @@
 /** @type {import('@dreamlab.gg/core').LooseSpawnableDefinition[]} */
 export const balls = [
-    {
-      entity: '@dreamlab/Solid', // bottom floor
-      args: [5_000, 50],
-      transform: {
-        position: { x: 0, y: 2_000 },
-        rotation: 0,
-      },
+  {
+    entity: '@dreamlab/Solid', // bottom floor
+    args: [5_000, 50],
+    transform: {
+      position: { x: 0, y: 2_000 },
+      rotation: 0,
     },
-    {
-      entity: '@dreamlab/Solid', // bottom mob floor wall left
-      args: [300, 50],
+  },
+  {
+    entity: '@dreamlab/Solid', // bottom mob floor wall left
+    args: [300, 50],
+    transform: {
+      position: { x: -1_500, y: 2_000 },
+      rotation: 90,
+    },
+  },
+  {
+    entity: '@dreamlab/Solid', // bottom mob floor wall right
+    args: [300, 50],
+    transform: {
+      position: { x: 1_500, y: 2_000 },
+      rotation: 90,
+    },
+  },
+  ...Array.from({ length: 50 })
+    .fill(null)
+    .map(() => ({
+      entity: '@dreamlab/BouncyBall',
+      args: [50],
       transform: {
-        position: { x: -1_500, y: 2_000 },
+        position: { x: 0, y: 1_750 },
         rotation: 90,
       },
-    },
-    {
-      entity: '@dreamlab/Solid', // bottom mob floor wall right
-      args: [300, 50],
-      transform: {
-        position: { x: 1_500, y: 2_000 },
-        rotation: 90,
-      },
-    },
-    ...Array.from({ length: 50 })
-      .fill(null)
-      .map(() => ({
-        entity: '@dreamlab/BouncyBall',
-        args: [50],
-        transform: {
-          position: { x: 0, y: 1_750 },
-          rotation: 90,
-        },
-      })),
-  ]
-  
+    })),
+]
