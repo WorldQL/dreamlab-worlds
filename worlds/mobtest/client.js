0dc4fd6ef3f34591660391af4fd5de2ab8eaac77
CodedRed <devcodedred@gmail.com>
2023-09-20 13:17:25 -0500

    hide graphics until we get better ones

--- worlds/mobtest/client.js
+++ worlds/mobtest/client.js
@@ -4,7 +4,7 @@ import { createBackground } from './dist/entities/client/background.js'
 import { createFreeform } from './dist/entities/client/freeform.js'
 import { createGrapplingHook } from './dist/entities/client/grapplingHook.js'
 import { createPlatform } from './dist/entities/client/platform.js'
-import { images } from './level/client/graphics.js'
+// import { images } from './level/client/graphics.js'
 import { map1 } from './level/client/map1.js'
 
 /** @type {import('@dreamlab.gg/core/sdk').InitClient} */
@@ -17,5 +17,6 @@ export const init = async game => {
   game.register('@dreamlab/Platform', createPlatform)
 
   await sharedInit(game)
-  await game.spawnMany(...images, ...map1)
+  await game.spawn(...map1)
+  // await game.spawn(...images)
 }
