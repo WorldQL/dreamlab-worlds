487a5df502f580dba375fb664a2575a21f159b70
CodedRed <devcodedred@gmail.com>
2023-09-25 14:14:18 -0500

    implemented cycle weapon ui

--- worlds/mobtest/client.tsx
+++ worlds/mobtest/client.tsx
@@ -1,4 +1,9 @@
-import React, { FC, useEffect, useRef } from 'https://esm.sh/react@18.2.0'
+import React, {
+  FC,
+  useEffect,
+  useRef,
+  useState,
+} from 'https://esm.sh/react@18.2.0'
 import { createRoot } from 'https://esm.sh/react-dom@18.2.0/client'
 import { Texture, Application, Sprite } from 'pixi.js'
 import { isPlayer } from '@dreamlab.gg/core/dist/entities'
@@ -19,15 +24,16 @@ const HotbarItem: FC<HotbarItemProps> = ({ item, isSelected }) => {
   useEffect(() => {
     if (ref.current && item.image) {
       const sprite = new Sprite(item.image)
-      sprite.width = 40
-      sprite.height = 40
+      sprite.width = 90
+      sprite.height = 90
       sprite.x = 0
       sprite.y = 0
 
       const app = new Application({
-        width: 40,
-        height: 40,
+        width: 90,
+        height: 90,
         view: ref.current,
+        background: '#fff',
       })
 
       app.stage.addChild(sprite)
@@ -41,8 +47,8 @@ const HotbarItem: FC<HotbarItemProps> = ({ item, isSelected }) => {
   return (
     <canvas
       ref={ref}
-      width='40'
-      height='40'
+      width='90'
+      height='90'
       style={{
         border: isSelected ? '2px solid blue' : '1px solid gray',
         marginRight: '4px',
@@ -83,14 +89,43 @@ const Hotbar: FC<HotbarProps> = ({ items, currentItemIndex }) => (
 
 interface AppProps {
   items: Item[]
-  currentItemIndex: number
+  initialCurrentItemIndex: number
+  game: any
 }
 
-const App: FC<AppProps> = ({ items, currentItemIndex }) => (
-  <div style={{ position: 'relative', height: '100vh' }}>
-    <Hotbar items={items} currentItemIndex={currentItemIndex} />
-  </div>
-)
+const App: FC<AppProps> = ({ items, initialCurrentItemIndex, game }) => {
+  const [currentItemIndex, setCurrentItemIndex] = useState(
+    initialCurrentItemIndex,
+  )
+  let didRespondToCycleItem = false
+
+  useEffect(() => {
+    const checkInputAndCycleItem = () => {
+      const inputs = game.client?.inputs
+      const shouldCycle = inputs?.getInput('@player/cycle-item') ?? false
+
+      if (shouldCycle) {
+        if (!didRespondToCycleItem) {
+          setCurrentItemIndex(prevIndex => (prevIndex + 1) % items.length)
+          didRespondToCycleItem = true
+        }
+      } else {
+        didRespondToCycleItem = false
+      }
+      requestAnimationFrame(checkInputAndCycleItem)
+    }
+
+    requestAnimationFrame(checkInputAndCycleItem)
+
+    return () => {}
+  }, [items, game])
+
+  return (
+    <div style={{ position: 'relative', height: '100vh' }}>
+      <Hotbar items={items} currentItemIndex={currentItemIndex} />
+    </div>
+  )
+}
 
 const setupInventory = (game: {
   entities: any[]
@@ -105,7 +140,13 @@ const setupInventory = (game: {
     const uiContainer = document.createElement('div')
     game.client.ui.add(uiContainer)
     const reactRoot = createRoot(uiContainer)
-    reactRoot.render(<App items={items} currentItemIndex={currentItemIndex} />)
+    reactRoot.render(
+      <App
+        items={items}
+        initialCurrentItemIndex={currentItemIndex}
+        game={game}
+      />,
+    )
   }
 }
 
