f30887702b666f6c831447ef2de0adf9dafe96b3
Jack Baron <jackmbaron@gmail.com>
2023-10-17 21:09:44 +0100

    update passive mob

--- worlds/mobtest/src/entities/shared/mobs/passive.ts
+++ worlds/mobtest/src/entities/shared/mobs/passive.ts
@@ -1,252 +1,279 @@
-import type { SpawnableEntity } from '@dreamlab.gg/core'
+import type { Game, SpawnableEntity } from '@dreamlab.gg/core'
 import { createSpawnableEntity } from '@dreamlab.gg/core'
+import { z } from '@dreamlab.gg/core/dist/sdk'
+import type { Camera } from '@dreamlab.gg/core/entities'
 import { isNetPlayer } from '@dreamlab.gg/core/entities'
 import { cloneTransform, Vec } from '@dreamlab.gg/core/math'
-import type { NetClient } from '@dreamlab.gg/core/network'
 import { onlyNetClient, onlyNetServer } from '@dreamlab.gg/core/network'
+import type {
+  MessageListenerClient,
+  MessageListenerServer,
+  NetClient,
+  NetServer,
+} from '@dreamlab.gg/core/network'
 import { drawBox, drawCircle } from '@dreamlab.gg/core/utils'
 import Matter from 'matter-js'
 import { Container, Graphics } from 'pixi.js'
 
-export const createPassiveMob = createSpawnableEntity(
-  ({ uid, tags, transform, zIndex }) => {
-    const HIT_CHANNEL = '@dreamlab/Hittable/hit'
-    const { position } = transform
+const ArgsSchema = z.object({})
 
-    const width = 130
-    const height = width * 2
-    const body = Matter.Bodies.rectangle(position.x, position.y, width, height)
+interface Data {
+  game: Game<boolean>
+  body: Matter.Body
+  onHitServer: MessageListenerServer
+  onHitClient: MessageListenerClient
+  netServer: NetServer | undefined
+  netClient: NetClient | undefined
+}
 
-    const hitRadius = width / 2 + 120
-    const hitCooldown = 1 // Second(s)
-    let hitCooldownCounter = 0
+interface Render {
+  camera: Camera
+  container: Container
+  gfxBounds: Graphics
+  gfxHittest: Graphics
+  ctrHealth: Container
+  gfxHealthAmount: Graphics
+}
 
-    const healthIndicatorWidth = width + 50
-    const healthIndicatorHeight = 20
+export const createPassiveMob = createSpawnableEntity<
+  typeof ArgsSchema,
+  SpawnableEntity<Data, Render>,
+  Data,
+  Render
+>(ArgsSchema, ({ uid, tags, transform, zIndex }) => {
+  const HIT_CHANNEL = '@dreamlab/Hittable/hit'
+  const { position } = transform
 
-    const maxHealth = 10
-    let health = maxHealth
+  const width = 130
+  const height = width * 2
+  const body = Matter.Bodies.rectangle(position.x, position.y, width, height)
 
-    let direction = 1
+  const hitRadius = width / 2 + 120
+  const hitCooldown = 1 // Second(s)
+  let hitCooldownCounter = 0
 
-    const onPlayerAttack: (
-      playerBody: Matter.Body,
-      animation: string,
-      netClient?: NetClient,
-    ) => void = (playerBody, _, netClient) => {
-      if (hitCooldownCounter <= 0) {
-        const xDiff = playerBody.position.x - body.position.x
+  const healthIndicatorWidth = width + 50
+  const healthIndicatorHeight = 20
 
-        if (Math.abs(xDiff) <= hitRadius) {
-          netClient?.sendCustomMessage(HIT_CHANNEL, { uid })
-          hitCooldownCounter = hitCooldown * 60
-        }
+  const maxHealth = 10
+  let health = maxHealth
+
+  let direction = 1
+
+  const onPlayerAttack: (
+    playerBody: Matter.Body,
+    animation: string,
+    netClient?: NetClient,
+  ) => void = (playerBody, _, netClient) => {
+    if (hitCooldownCounter <= 0) {
+      const xDiff = playerBody.position.x - body.position.x
+
+      if (Math.abs(xDiff) <= hitRadius) {
+        netClient?.sendCustomMessage(HIT_CHANNEL, { uid })
+        hitCooldownCounter = hitCooldown * 60
       }
     }
+  }
 
-    const onCollisionStart = (
-      pair: readonly [a: SpawnableEntity, b: SpawnableEntity],
-    ) => {
-      const [a, b] = pair
-      if (a.uid === uid || b.uid === uid) {
-        direction = -direction
+  const onCollisionStart = (
+    pair: readonly [a: SpawnableEntity, b: SpawnableEntity],
+  ) => {
+    const [a, b] = pair
+    if (a.uid === uid || b.uid === uid) {
+      direction = -direction
+    }
+  }
+
+  return {
+    get tags() {
+      return tags
+    },
+
+    get transform() {
+      return cloneTransform(transform)
+    },
+
+    isInBounds(position) {
+      return Matter.Query.point([body], position).length > 0
+    },
+
+    init({ game }) {
+      game.physics.register(this, body)
+
+      const netServer = onlyNetServer(game)
+      const netClient = onlyNetClient(game)
+
+      game.events.common.addListener(
+        'onPlayerAttack',
+        (playerBody, animation) =>
+          onPlayerAttack(playerBody, animation, netClient),
+      )
+      game.events.common.addListener('onCollisionStart', onCollisionStart)
+
+      const onHitServer: MessageListenerServer = async (
+        { peerID },
+        _,
+        data,
+      ) => {
+        const network = netServer
+        if (!network) throw new Error('missing network')
+
+        if (!('uid' in data)) return
+        if (typeof data.uid !== 'string') return
+        if (data.uid !== uid) return
+
+        const player = game.entities
+          .filter(isNetPlayer)
+          .find(netplayer => netplayer.peerID === peerID)
+
+        if (!player) throw new Error('missing netplayer')
+
+        direction = body.position.x > player.position.x ? 1 : -1
+        const force = 0.5 * direction
+        Matter.Body.applyForce(body, body.position, { x: force, y: -1.75 })
+
+        health -= 1
+        if (health <= 0) {
+          const respawnPosition = { ...body.position }
+
+          // @ts-expect-error `this` is a partial entity
+          await game.destroy(this)
+
+          setTimeout(async () => {
+            await game.spawn({
+              entity: '@dreamlab/PassiveMob',
+              args: {},
+              transform: { position: [respawnPosition.x, respawnPosition.y] },
+              tags: ['net/replicated'],
+            })
+          }, 10_000)
+        } else {
+          network.broadcastCustomMessage(HIT_CHANNEL, { uid, health })
+        }
       }
-    }
 
-    return {
-      get tags() {
-        return tags
-      },
+      const onHitClient: MessageListenerClient = (_, data) => {
+        const network = netClient
+        if (!network) throw new Error('missing network')
 
-      get transform() {
-        return cloneTransform(transform)
-      },
+        if (!('uid' in data)) return
+        if (typeof data.uid !== 'string') return
+        if (data.uid !== uid) return
 
-      isInBounds(position) {
-        return Matter.Query.point([body], position).length > 0
-      },
+        if (!('health' in data)) return
+        if (typeof data.health !== 'number') return
 
-      init({ game }) {
-        game.physics.register(this, body)
+        health = data.health
+      }
 
-        const netServer = onlyNetServer(game)
-        const netClient = onlyNetClient(game)
+      netServer?.addCustomMessageListener(HIT_CHANNEL, onHitServer)
+      netClient?.addCustomMessageListener(HIT_CHANNEL, onHitClient)
 
-        game.events.common.addListener(
-          'onPlayerAttack',
-          (playerBody, animation) =>
-            onPlayerAttack(playerBody, animation, netClient),
-        )
-        game.events.common.addListener('onCollisionStart', onCollisionStart)
+      return { game, body, onHitServer, onHitClient, netServer, netClient }
+    },
 
-        const onHitServer = async (
-          { peerID }: { peerID: string },
-          _: unknown,
-          data: { uid: string },
-        ) => {
-          const network = netServer
-          if (!network) throw new Error('missing network')
+    initRenderContext(_, { camera, stage }) {
+      const container = new Container()
+      container.sortableChildren = true
+      container.zIndex = zIndex
 
-          if (!('uid' in data)) return
-          if (typeof data.uid !== 'string') return
-          if (data.uid !== uid) return
+      const gfxBounds = new Graphics()
+      const gfxHittest = new Graphics()
 
-          const player = game.entities
-            .filter(isNetPlayer)
-            .find(netplayer => netplayer.peerID === peerID)
+      const ctrHealth = new Container()
+      ctrHealth.sortableChildren = true
 
-          if (!player) throw new Error('missing netplayer')
+      gfxHittest.zIndex = -1
+      ctrHealth.zIndex = 1
+      ctrHealth.position.y = -height / 2 - 30
 
-          direction = body.position.x > player.position.x ? 1 : -1
-          const force = 0.5 * direction
-          Matter.Body.applyForce(body, body.position, { x: force, y: -1.75 })
+      drawBox(gfxBounds, { width, height }, { stroke: '#00f' })
+      drawCircle(
+        gfxHittest,
+        { radius: hitRadius },
+        { fill: 'red', fillAlpha: 1, strokeAlpha: 0 },
+      )
 
-          health -= 1
-          if (health <= 0) {
-            const respawnPosition = { ...body.position }
+      const gfxHealthBorder = new Graphics()
+      const gfxHealthAmount = new Graphics()
 
-            // @ts-expect-error `this` is a partial entity
-            await game.destroy(this)
+      drawBox(
+        gfxHealthBorder,
+        { width: healthIndicatorWidth, height: healthIndicatorHeight },
+        {
+          fill: 'white',
+          stroke: 'black',
+          fillAlpha: 1,
+          strokeAlign: 1,
+          strokeWidth: 4,
+        },
+      )
 
-            setTimeout(async () => {
-              await game.spawn({
-                entity: '@dreamlab/PassiveMob',
-                args: [],
-                transform: { position: [respawnPosition.x, respawnPosition.y] },
-                tags: ['net/replicated'],
-              })
-            }, 10_000)
-          } else {
-            network.broadcastCustomMessage(HIT_CHANNEL, { uid, health })
-          }
-        }
+      ctrHealth.addChild(gfxHealthBorder)
+      ctrHealth.addChild(gfxHealthAmount)
 
-        const onHitClient = (
-          _: unknown,
-          data: { uid: string; health: number },
-        ) => {
-          const network = netClient
-          if (!network) throw new Error('missing network')
+      container.addChild(gfxBounds)
+      container.addChild(gfxHittest)
+      container.addChild(ctrHealth)
+      stage.addChild(container)
 
-          if (!('uid' in data)) return
-          if (typeof data.uid !== 'string') return
-          if (data.uid !== uid) return
+      return {
+        camera,
+        container,
+        gfxBounds,
+        gfxHittest,
+        ctrHealth,
+        gfxHealthAmount,
+      }
+    },
 
-          if (!('health' in data)) return
-          if (typeof data.health !== 'number') return
+    teardown({ game, onHitServer, onHitClient, netServer, netClient }) {
+      game.physics.unregister(this, body)
+      game.events.common.removeListener('onPlayerAttack', onPlayerAttack)
+      game.events.common.removeListener('onCollisionStart', onCollisionStart)
 
-          health = data.health
-        }
+      netServer?.removeCustomMessageListener(HIT_CHANNEL, onHitServer)
+      netClient?.removeCustomMessageListener(HIT_CHANNEL, onHitClient)
+    },
 
-        netServer?.addCustomMessageListener(HIT_CHANNEL, onHitServer)
-        netClient?.addCustomMessageListener(HIT_CHANNEL, onHitClient)
+    teardownRenderContext({ container, ctrHealth }) {
+      container.destroy({ children: true })
+      ctrHealth.destroy({ children: true })
+    },
 
-        return { game, body, onHitServer, onHitClient, netServer, netClient }
-      },
+    onPhysicsStep(_) {
+      Matter.Body.setAngle(body, 0)
+      Matter.Body.setAngularVelocity(body, 0)
 
-      initRenderContext(_, { camera, stage }) {
-        const container = new Container()
-        container.sortableChildren = true
-        container.zIndex = zIndex
+      if (hitCooldownCounter > 0) {
+        hitCooldownCounter -= 1
+      }
 
-        const gfxBounds = new Graphics()
-        const gfxHittest = new Graphics()
+      const speed = 2
+      Matter.Body.translate(body, { x: speed * direction, y: 0 })
+    },
 
-        const ctrHealth = new Container()
-        ctrHealth.sortableChildren = true
+    onRenderFrame(
+      { smooth },
+      { game },
+      { camera, container, gfxHittest, gfxBounds, gfxHealthAmount },
+    ) {
+      const debug = game.debug
+      const smoothed = Vec.add(body.position, Vec.mult(body.velocity, smooth))
+      const pos = Vec.add(smoothed, camera.offset)
 
-        gfxHittest.zIndex = -1
-        ctrHealth.zIndex = 1
-        ctrHealth.position.y = -height / 2 - 30
+      container.position = pos
+      container.rotation = body.angle
 
-        drawBox(gfxBounds, { width, height }, { stroke: '#00f' })
-        drawCircle(
-          gfxHittest,
-          { radius: hitRadius },
-          { fill: 'red', fillAlpha: 1, strokeAlpha: 0 },
-        )
+      const alpha = debug.value ? 0.5 : 0
+      gfxBounds.alpha = alpha
+      gfxHittest.alpha = hitCooldownCounter === 0 ? alpha / 3 : 0
 
-        const gfxHealthBorder = new Graphics()
-        const gfxHealthAmount = new Graphics()
-
-        drawBox(
-          gfxHealthBorder,
-          { width: healthIndicatorWidth, height: healthIndicatorHeight },
-          {
-            fill: 'white',
-            stroke: 'black',
-            fillAlpha: 1,
-            strokeAlign: 1,
-            strokeWidth: 4,
-          },
-        )
-
-        ctrHealth.addChild(gfxHealthBorder)
-        ctrHealth.addChild(gfxHealthAmount)
-
-        container.addChild(gfxBounds)
-        container.addChild(gfxHittest)
-        container.addChild(ctrHealth)
-        stage.addChild(container)
-
-        return {
-          camera,
-          container,
-          gfxBounds,
-          gfxHittest,
-          ctrHealth,
-          gfxHealthAmount,
-        }
-      },
-
-      teardown({ game, onHitServer, onHitClient, netServer, netClient }) {
-        game.physics.unregister(this, body)
-        game.events.common.removeListener('onPlayerAttack', onPlayerAttack)
-        game.events.common.removeListener('onCollisionStart', onCollisionStart)
-
-        netServer?.removeCustomMessageListener(HIT_CHANNEL, onHitServer)
-        netClient?.removeCustomMessageListener(HIT_CHANNEL, onHitClient)
-      },
-
-      teardownRenderContext({ container, ctrHealth }) {
-        container.destroy({ children: true })
-        ctrHealth.destroy({ children: true })
-      },
-
-      onPhysicsStep(_) {
-        Matter.Body.setAngle(body, 0)
-        Matter.Body.setAngularVelocity(body, 0)
-
-        if (hitCooldownCounter > 0) {
-          hitCooldownCounter -= 1
-        }
-
-        const speed = 2
-        Matter.Body.translate(body, { x: speed * direction, y: 0 })
-      },
-
-      onRenderFrame(
-        { smooth },
-        { game },
-        { camera, container, gfxHittest, gfxBounds, gfxHealthAmount },
-      ) {
-        const debug = game.debug
-        const smoothed = Vec.add(body.position, Vec.mult(body.velocity, smooth))
-        const pos = Vec.add(smoothed, camera.offset)
-
-        container.position = pos
-        container.rotation = body.angle
-
-        const alpha = debug.value ? 0.5 : 0
-        gfxBounds.alpha = alpha
-        gfxHittest.alpha = hitCooldownCounter === 0 ? alpha / 3 : 0
-
-        drawBox(
-          gfxHealthAmount,
-          { width: (health / maxHealth) * healthIndicatorWidth, height: 20 },
-          { fill: 'red', fillAlpha: 1, strokeAlpha: 0 },
-        )
-      },
-    }
-  },
-)
+      drawBox(
+        gfxHealthAmount,
+        { width: (health / maxHealth) * healthIndicatorWidth, height: 20 },
+        { fill: 'red', fillAlpha: 1, strokeAlpha: 0 },
+      )
+    },
+  }
+})
