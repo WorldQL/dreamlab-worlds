bd4d90b5d5bec8104ed4b8f07351a188676b5baa
CodedRed <devcodedred@gmail.com>
2023-10-24 15:09:04 -0500

    implemented small zombie game

--- worlds/survival/src/server.ts
+++ worlds/survival/src/server.ts
@@ -7,22 +7,35 @@ export const init: InitServer = async game => {
   await sharedInit(game)
 
   const PLAY_CHANNEL = 'game/start'
-
   const netServer = onlyNetServer(game)
 
-  const onStartGame: MessageListenerServer = async () => {
+  let spawnInterval = 30 * 1_000
+  let spawnTimeout
+
+  const spawnZombies = async () => {
     await game.spawn({
-      entity: '@dreamlab/ArcherMob',
+      entity: '@dreamlab/ZombieMob',
       args: {},
       transform: { position: [-1_250, 300] },
       tags: ['net/replicated'],
     })
+
     await game.spawn({
-      entity: '@dreamlab/ArcherMob',
+      entity: '@dreamlab/ZombieMob',
       args: {},
       transform: { position: [1_000, 300] },
       tags: ['net/replicated'],
     })
+
+    spawnInterval *= 0.9
+
+    spawnInterval = Math.max(5 * 1_000, spawnInterval)
+
+    spawnTimeout = setTimeout(spawnZombies, spawnInterval)
+  }
+
+  const onStartGame: MessageListenerServer = async () => {
+    await spawnZombies()
   }
 
   netServer?.addCustomMessageListener(PLAY_CHANNEL, onStartGame)
