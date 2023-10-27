00b0d3f1a03d856f746c7a01adbe06be4a66847c
CodedRed <devcodedred@gmail.com>
2023-10-27 12:23:46 -0500

    level changes

--- worlds/survival/src/level/shared/level.ts
+++ worlds/survival/src/level/shared/level.ts
@@ -3,9 +3,196 @@ import type { LooseSpawnableDefinition } from '@dreamlab.gg/core'
 export const level: LooseSpawnableDefinition[] = [
   {
     entity: '@dreamlab/Solid', // spawn land
-    args: { width: 10_000, height: 150 },
+    args: { width: 3_000, height: 150 },
     transform: {
-      position: { x: 0, y: 600 },
+      position: { x: 800, y: 600 },
+      rotation: 0,
+    },
+  },
+  {
+    entity: '@dreamlab/Solid', // large right wall
+    args: { width: 3_000, height: 500 },
+    transform: {
+      position: { x: 2_550, y: -825 },
+      rotation: 90,
+    },
+  },
+  {
+    entity: '@dreamlab/Solid', // right well stand
+    args: { width: 100, height: 150 },
+    transform: {
+      position: { x: -625, y: 475 },
+      rotation: 90,
+    },
+  },
+  {
+    entity: '@dreamlab/Solid', // left well stand
+    args: { width: 100, height: 150 },
+    transform: {
+      position: { x: -1_025, y: 475 },
+      rotation: 90,
+    },
+  },
+  {
+    entity: '@dreamlab/Solid', // left well floor
+    args: { width: 1_000, height: 150 },
+    transform: {
+      position: { x: -1_450, y: 600 },
+      rotation: 0,
+    },
+  },
+  {
+    entity: '@dreamlab/Nonsolid', // well left pillar
+    args: { width: 500, height: 25 },
+    transform: {
+      position: { x: -1_025, y: 175 },
+      rotation: 90,
+    },
+  },
+  {
+    entity: '@dreamlab/Nonsolid', // well right pillar
+    args: { width: 500, height: 25 },
+    transform: {
+      position: { x: -625, y: 175 },
+      rotation: 90,
+    },
+  },
+  {
+    entity: '@dreamlab/Nonsolid', // well top
+    args: { width: 500, height: 100 },
+    transform: {
+      position: { x: -825, y: -125 },
+      rotation: 0,
+    },
+  },
+  {
+    entity: '@dreamlab/BreakableSolid', // well wooden planks
+    args: { width: 265, height: 50 },
+    transform: {
+      position: { x: -890, y: 450 },
+      rotation: 0,
+    },
+  },
+  {
+    entity: '@dreamlab/Solid', // well left down
+    args: { width: 3_000, height: 100 },
+    transform: {
+      position: { x: -1_000, y: 2_170 },
+      rotation: 90,
+    },
+  },
+  {
+    entity: '@dreamlab/Solid', // well right down
+    args: { width: 3_000, height: 100 },
+    transform: {
+      position: { x: -650, y: 2_170 },
+      rotation: 90,
+    },
+  },
+  {
+    entity: '@dreamlab/Solid', // large left wall
+    args: { width: 3_000, height: 500 },
+    transform: {
+      position: { x: -2_200, y: -825 },
+      rotation: 90,
+    },
+  },
+  {
+    entity: '@dreamlab/Nonsolid', // weapon 1 pillar
+    args: { width: 200, height: 100 },
+    transform: {
+      position: { x: 800, y: 475 },
+      rotation: 0,
+    },
+  },
+  {
+    entity: '@dreamlab/Nonsolid', // weapon 2 pillar
+    args: { width: 200, height: 100 },
+    transform: {
+      position: { x: 1_300, y: 475 },
+      rotation: 0,
+    },
+  },
+  {
+    entity: '@dreamlab/Nonsolid', // weapon 3 pillar
+    args: { width: 200, height: 100 },
+    transform: {
+      position: { x: 1_800, y: 475 },
+      rotation: 0,
+    },
+  },
+  {
+    entity: '@dreamlab/PickupItem', // weapon 1
+    args: {
+      width: 200,
+      height: 200,
+      spriteSource: '/worlds/mobtest/assets/img/bow.png',
+      itemDisplayName: 'Gold Bow',
+      animationName: 'bow',
+    },
+    transform: {
+      position: { x: 800, y: 300 },
+      rotation: 0,
+    },
+  },
+  {
+    entity: '@dreamlab/PickupItem', // weapon 2
+    args: {
+      width: 200,
+      height: 200,
+      spriteSource: '/worlds/mobtest/assets/img/bow.png',
+      itemDisplayName: 'Gold Bow',
+      animationName: 'bow',
+    },
+    transform: {
+      position: { x: 1_300, y: 300 },
+      rotation: 0,
+    },
+  },
+  {
+    entity: '@dreamlab/PickupItem', // weapon 3
+    args: {
+      width: 200,
+      height: 200,
+      spriteSource: '/worlds/mobtest/assets/img/bow.png',
+      itemDisplayName: 'Gold Bow',
+      animationName: 'bow',
+    },
+    transform: {
+      position: { x: 1_800, y: 300 },
+      rotation: 0,
+    },
+  },
+  // BOSS LEVEL BELOW
+  {
+    entity: '@dreamlab/Solid', // left wall
+    args: { width: 1_000, height: 100 },
+    transform: {
+      position: { x: -1_000, y: 4_170 },
+      rotation: 90,
+    },
+  },
+  {
+    entity: '@dreamlab/Solid', // floor
+    args: { width: 3_000, height: 100 },
+    transform: {
+      position: { x: 550, y: 4_620 },
+      rotation: 0,
+    },
+  },
+  {
+    entity: '@dreamlab/Solid', // right wall
+    args: { width: 1_000, height: 100 },
+    transform: {
+      position: { x: 2_100, y: 4_170 },
+      rotation: 90,
+    },
+  },
+  {
+    entity: '@dreamlab/Solid', // ceiling
+    args: { width: 2_750, height: 100 },
+    transform: {
+      position: { x: 775, y: 3_620 },
       rotation: 0,
     },
   },
