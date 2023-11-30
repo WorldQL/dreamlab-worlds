669b67691dabc0f45206f4cc9223d1f18271648d
Christopher G <devcodedred@gmail.com>
2023-11-30 15:54:19 -0600

    optimized

--- worlds/survival/src/managers/shooters.ts
+++ worlds/survival/src/managers/shooters.ts
@@ -13,14 +13,13 @@ const delay = async (ms: number | undefined) =>
     setTimeout(resolve, ms)
   })
 
-const Y_OFFSET_DEFAULT = 75
 const ATTACK_COOLDOWN = 250
-const SHOT_DELAY = 100
 const SHOOT_CHANNEL = '@dreamlab/Projectile/fired'
+const SHOT_DELAY = 100
+const Y_OFFSET_DEFAULT = 75
 
 export const initProjectileWeapons = (game: Game<false>) => {
   let lastSpawnedTime: number | null = null
-
   const netClient = onlyNetClient(game)
   const netServer = onlyNetServer(game)
 
@@ -31,17 +30,8 @@ export const initProjectileWeapons = (game: Game<false>) => {
     angleOffset: number,
     yOffset = Y_OFFSET_DEFAULT,
   ) => {
-    const xOffsetFactor = 165
-    const xOffset = direction === 1 ? xOffsetFactor : -xOffsetFactor
-
-    let additionalOffsetX = 0
-    let additionalOffsetY = 0
-
-    if (animation === 'shoot') {
-      additionalOffsetX = 0
-      additionalOffsetY = -50
-    }
-
+    const xOffset = direction === 1 ? 165 : -165
+    const additionalOffsetY = animation === 'shoot' ? -50 : 0
     return game.spawn({
       entity: '@dreamlab/Projectile',
       args: { width: 50, height: 10, direction },
@@ -57,21 +47,18 @@ export const initProjectileWeapons = (game: Game<false>) => {
   }
 
   const onHitServer: MessageListenerServer = async ({ peerID }, _, data) => {
-    const network = netServer
-    if (!network) throw new Error('missing network')
-
+    if (!netServer) throw new Error('missing network')
     const player = game.entities
       .filter(isNetPlayer)
       .find(netplayer => netplayer.peerID === peerID)
-
     if (!player) throw new Error('missing netplayer')
-
-    const direction = data.direction as number
-    const animation = data.animation as string
-    const position = data.position as [number, number]
-    const angle = data.angle as number
-    const yOffset = data.yOffset ? (data.yOffset as number) : undefined
-
+    const { direction, animation, position, angle, yOffset } = data as {
+      direction: number
+      animation: string
+      position: [number, number]
+      angle: number
+      yOffset?: number
+    }
     await spawnProjectile(direction, animation, position, angle, yOffset)
   }
 
@@ -85,143 +72,62 @@ export const initProjectileWeapons = (game: Game<false>) => {
         player.currentAnimation === 'shoot'
       ) {
         const currentTime = Date.now()
-        if (
-          !lastSpawnedTime ||
-          currentTime - lastSpawnedTime > ATTACK_COOLDOWN
-        ) {
+        if (!lastSpawnedTime || currentTime - lastSpawnedTime > ATTACK_COOLDOWN)
           lastSpawnedTime = currentTime
-        } else {
-          return
-        }
+        else return
 
         const invItem = InventoryManager.getInstance().getItemFromItem(gear)
 
+        const sendProjectileMessage = (angle: number, yOffset?: number) =>
+          netClient?.sendCustomMessage(SHOOT_CHANNEL, {
+            direction: player.facingDirection,
+            animation: player.currentAnimation,
+            position: [player.body.position.x, player.body.position.y],
+            angle,
+            yOffset,
+          })
+
         switch (invItem?.projectileType) {
           case ProjectileTypes.SINGLE_SHOT:
-            netClient?.sendCustomMessage(SHOOT_CHANNEL, {
-              direction: player.facingDirection,
-              animation: player.currentAnimation,
-              position: [player.body.position.x, player.body.position.y],
-              angle: 0,
-            })
-
-            break
-
           case ProjectileTypes.DOUBLE_SHOT:
-            netClient?.sendCustomMessage(SHOOT_CHANNEL, {
-              direction: player.facingDirection,
-              animation: player.currentAnimation,
-              position: [player.body.position.x, player.body.position.y],
-              angle: 0,
-            })
-            await delay(SHOT_DELAY)
-            netClient?.sendCustomMessage(SHOOT_CHANNEL, {
-              direction: player.facingDirection,
-              animation: player.currentAnimation,
-              position: [player.body.position.x, player.body.position.y],
-              angle: 0,
-            })
-            break
-
           case ProjectileTypes.BURST_SHOT:
-            netClient?.sendCustomMessage(SHOOT_CHANNEL, {
-              direction: player.facingDirection,
-              animation: player.currentAnimation,
-              position: [player.body.position.x, player.body.position.y],
-              angle: 0,
-            })
-            await delay(SHOT_DELAY)
-            netClient?.sendCustomMessage(SHOOT_CHANNEL, {
-              direction: player.facingDirection,
-              animation: player.currentAnimation,
-              position: [player.body.position.x, player.body.position.y],
-              angle: 0,
-            })
-            await delay(SHOT_DELAY)
-            netClient?.sendCustomMessage(SHOOT_CHANNEL, {
-              direction: player.facingDirection,
-              animation: player.currentAnimation,
-              position: [player.body.position.x, player.body.position.y],
-              angle: 0,
-            })
-            break
+            for (
+              let idx = 0;
+              idx <
+              (invItem.projectileType === ProjectileTypes.SINGLE_SHOT
+                ? 1
+                : invItem.projectileType === ProjectileTypes.DOUBLE_SHOT
+                ? 2
+                : 3);
+              idx++
+            ) {
+              sendProjectileMessage(0)
+              // eslint-disable-next-line no-await-in-loop
+              if (idx < 2) await delay(SHOT_DELAY)
+            }
 
+            break
           case ProjectileTypes.SCATTER_SHOT:
-            netClient?.sendCustomMessage(SHOOT_CHANNEL, {
-              direction: player.facingDirection,
-              animation: player.currentAnimation,
-              position: [player.body.position.x, player.body.position.y],
-              angle: 0.1,
-              yOffset: 70,
-            })
-            netClient?.sendCustomMessage(SHOOT_CHANNEL, {
-              direction: player.facingDirection,
-              animation: player.currentAnimation,
-              position: [player.body.position.x, player.body.position.y],
-              angle: 0,
-            })
-            netClient?.sendCustomMessage(SHOOT_CHANNEL, {
-              direction: player.facingDirection,
-              animation: player.currentAnimation,
-              position: [player.body.position.x, player.body.position.y],
-              angle: -1,
-              yOffset: 80,
-            })
-            break
+          case ProjectileTypes.DOUBLE_SCATTER_SHOT: {
+            const scatterShots =
+              invItem.projectileType === ProjectileTypes.SCATTER_SHOT ? 1 : 2
+            for (let idx = 0; idx < scatterShots; idx++) {
+              sendProjectileMessage(0.1, 70)
+              sendProjectileMessage(0)
+              sendProjectileMessage(-0.1, 80)
+              // eslint-disable-next-line no-await-in-loop
+              if (idx < 1) await delay(SHOT_DELAY)
+            }
 
-          case ProjectileTypes.DOUBLE_SCATTER_SHOT:
-            netClient?.sendCustomMessage(SHOOT_CHANNEL, {
-              direction: player.facingDirection,
-              animation: player.currentAnimation,
-              position: [player.body.position.x, player.body.position.y],
-              angle: 0.1,
-              yOffset: 70,
-            })
-            netClient?.sendCustomMessage(SHOOT_CHANNEL, {
-              direction: player.facingDirection,
-              animation: player.currentAnimation,
-              position: [player.body.position.x, player.body.position.y],
-              angle: 0,
-            })
-            netClient?.sendCustomMessage(SHOOT_CHANNEL, {
-              direction: player.facingDirection,
-              animation: player.currentAnimation,
-              position: [player.body.position.x, player.body.position.y],
-              angle: -0.1,
-              yOffset: 80,
-            })
-            await delay(SHOT_DELAY)
-
-            netClient?.sendCustomMessage(SHOOT_CHANNEL, {
-              direction: player.facingDirection,
-              animation: player.currentAnimation,
-              position: [player.body.position.x, player.body.position.y],
-              angle: 0.1,
-              yOffset: 70,
-            })
-            netClient?.sendCustomMessage(SHOOT_CHANNEL, {
-              direction: player.facingDirection,
-              animation: player.currentAnimation,
-              position: [player.body.position.x, player.body.position.y],
-              angle: 0,
-            })
-            netClient?.sendCustomMessage(SHOOT_CHANNEL, {
-              direction: player.facingDirection,
-              animation: player.currentAnimation,
-              position: [player.body.position.x, player.body.position.y],
-              angle: -0.1,
-              yOffset: 80,
-            })
             break
-          case undefined: {
-            throw new Error('Not implemented yet: undefined case')
           }
 
-          case ProjectileTypes.EXPLOSIVE_SHOT: {
+          case undefined:
+            throw new Error('Not implemented yet: undefined case')
+          case ProjectileTypes.EXPLOSIVE_SHOT:
             throw new Error(
               'Not implemented yet: ProjectileTypes.EXPLOSIVE_SHOT case',
             )
-          }
         }
       }
     },
