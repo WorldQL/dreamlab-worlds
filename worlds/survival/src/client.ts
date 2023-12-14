4293e50a9cff55bc20543f8c4ce4a5cc4fd93a64
Jackson Roberts <jacksonroberts25@gmail.com>
2023-12-14 09:22:29 -0700

    load player here instead.

--- worlds/survival/src/client.ts
+++ worlds/survival/src/client.ts
@@ -1,3 +1,4 @@
+import { deferUntilPlayer } from '@dreamlab.gg/core/dist/utils'
 import type { InitClient } from '@dreamlab.gg/core/sdk'
 import { preloadAssets } from './AssetLoader.js'
 import { initializeUI } from './screens/start.js'
@@ -6,6 +7,9 @@ import { sharedInit } from './shared.js'
 export const init: InitClient = async game => {
   await preloadAssets()
   initializeUI(game)
+  deferUntilPlayer(game, async player => {
+    await player.setCharacterId('c_y9ydqx2pghxl04emgnxu6r5g')
+  })
 
   await sharedInit(game)
 }
