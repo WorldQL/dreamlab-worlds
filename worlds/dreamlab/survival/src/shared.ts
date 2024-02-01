2040937f8c6bcf388f544147845fa2aa4da1dd50
Christopher G <devcodedred@gmail.com>
2024-01-31 20:28:43 -0600

    removed archer zombie (maybe we can re-implement another mob later)

--- worlds/dreamlab/survival/src/shared.ts
+++ worlds/dreamlab/survival/src/shared.ts
@@ -6,13 +6,11 @@ import { createGrappleHook } from './entities/spawnable/grappleHook.js'
 import { createInventoryItem } from './entities/spawnable/inventoryItem.js'
 import { createLadder } from './entities/spawnable/ladder.js'
 import { createZombieMob } from './entities/spawnable/mobs/zombie.js'
-import { createArcherMob } from './entities/spawnable/mobs/zombieArcher.js'
 import { createProjectile } from './entities/spawnable/projectile.js'
 import { createSpawnRegion } from './entities/spawnable/spawnRegion.js'
 
 export const sharedInit: InitShared = async game => {
   game.register('@cvz/ZombieMob', createZombieMob)
-  game.register('@cvz/ArcherMob', createArcherMob)
   game.register('@cvz/Hook', createGrappleHook)
   game.register('@cvz/Ladder', createLadder)
   game.register('@cvz/SpawnRegion', createSpawnRegion)
