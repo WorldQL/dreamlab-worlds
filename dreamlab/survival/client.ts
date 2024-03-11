092c5073257d1bb6563f338534c950e48b0cb67f
Christopher G <devcodedred@gmail.com>
2024-03-11 12:45:42 -0500

    set default zoom level

--- dreamlab/survival/client.ts
+++ dreamlab/survival/client.ts
@@ -17,5 +17,7 @@ export const init: InitClient = async game => {
     player.characterId = "c_y9ydqx2pghxl04emgnxu6r5g"
   })
 
+  game.client.render.camera.zoomScale = 20
+
   await sharedInit(game)
 }
